acd84c7bef0300296b30b5c35f3369d63fe29317
Edward.K <157359932+eddu-k@users.noreply.github.com>
2024-05-10 13:48:26 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,21 @@
+from asttokens import supports_tokenless
+import streamlit as st
+from tkinter import*
+import pylab
+from flask import Flask, render_template_string
+
+
+
+
+
+
+
+def main():
+    st.title('Hello!\n')
+    st.title('I am Edward.')
+    
+    
+
+
+if __name__ == "__main__":
+    main()
